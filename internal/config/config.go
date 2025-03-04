00a2a6fda0e4e59a0d6dcfb7152cbae83383da2f
neumann-mlucas <neumann.m.lucas@gmail.com>
2025-03-04 12:37:25 -0300

    feat: add active field to url model

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,34 +18,49 @@ var AppConfig *Config
 
 // LoadConfig initializes the application configuration and opens the SQLite in-memory DB
 func LoadConfig() error {
-	// Initialize the config struct
 	AppConfig = &Config{}
 
-	// Open an SQLite in-memory database
 	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
 		return fmt.Errorf("failed to open SQLite database: %w", err)
 	}
-
-	// Assign the *sql.DB instance to the AppConfig
 	AppConfig.DB = db
 
-	// Optionally, you can create tables here, for example:
 	if err := createTables(db); err != nil {
 		return fmt.Errorf("failed to create tables: %w", err)
 	}
+	return nil
+}
 
+// LoadTestConfig initializes the application configuration and opens the SQLite in-memory DB
+func LoadTestConfig() error {
+	AppConfig = &Config{}
+
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		return fmt.Errorf("failed to open SQLite database: %w", err)
+	}
+	AppConfig.DB = db
+
+	_, err = db.Exec("DROP TABLE IF EXISTS urls")
+	if err != nil {
+		return fmt.Errorf("failed to drop table: %w", err)
+	}
+
+	if err := createTables(db); err != nil {
+		return fmt.Errorf("failed to create tables: %w", err)
+	}
 	return nil
 }
 
 // createTables creates necessary tables in the SQLite in-memory database
 func createTables(db *sql.DB) error {
-	// Example table creation (you can add more as per your requirements)
 	query := `
     CREATE TABLE IF NOT EXISTS urls (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         hash TEXT NOT NULL,
-        url TEXT NOT NULL
+        url TEXT UNIQUE NOT NULL,
+		active BOOLEAN DEFAULT 1 NOT NULL
     );
     `
 	_, err := db.Exec(query)
--- internal/model/shorturl.go
+++ internal/model/shorturl.go
@@ -1,7 +1,8 @@
 package model
 
 type ShortUrl struct {
-	Id   int64  `json:"id"`
-	Hash string `json:"hash"`
-	Url  string `json:"url"`
+	Id     int64  `json:"id"`
+	Hash   string `json:"hash"`
+	Url    string `json:"url"`
+	Active bool
 }
